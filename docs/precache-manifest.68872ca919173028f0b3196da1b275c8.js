46d93e58cb767f1d0a855adbb7ebf8926634af87
BooBanker <72867484+boobank@users.noreply.github.com>
2020-11-26 20:43:03 +0000

    Add files via upload

---
+++ docs/precache-manifest.68872ca919173028f0b3196da1b275c8.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3bb47799c04b352a075801137aa1056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaabee6766b6036e5c09",
+    "url": "/static/css/main.b6533798.chunk.css"
+  },
+  {
+    "revision": "3973e9cdb9f0cde6fb56",
+    "url": "/static/js/0.6a51accc.chunk.js"
+  },
+  {
+    "revision": "ad1028476e39191cf201",
+    "url": "/static/js/10.8dfc8a1c.chunk.js"
+  },
+  {
+    "revision": "cbf7cdd1df57796cdd2d",
+    "url": "/static/js/3.a222f4b1.chunk.js"
+  },
+  {
+    "revision": "db1ad4e4b9187caada720e6eb871e26d",
+    "url": "/static/js/3.a222f4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee72bf4f6c5c9d2b0649",
+    "url": "/static/js/4.3dd6e8ae.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/static/js/4.3dd6e8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c358c36b9aec196626a",
+    "url": "/static/js/5.87541c3b.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/5.87541c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd34086e466708fe423d",
+    "url": "/static/js/6.d34bb556.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/6.d34bb556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3678cbeb65fce2cfe5",
+    "url": "/static/js/7.95cbbb05.chunk.js"
+  },
+  {
+    "revision": "4e3c3dfdfc4b2c2dfa4a",
+    "url": "/static/js/8.ec30a1a2.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.ec30a1a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cc779ede3e4f4c24eab",
+    "url": "/static/js/9.1064580e.chunk.js"
+  },
+  {
+    "revision": "eaabee6766b6036e5c09",
+    "url": "/static/js/main.ec3c3e73.chunk.js"
+  },
+  {
+    "revision": "acdd50cfbdf50a11c738",
+    "url": "/static/js/runtime-main.66c53b1c.js"
+  },
+  {
+    "revision": "41357bf047cd13ff3958a97bfe390fe1",
+    "url": "/static/media/Coinflip.41357bf0.png"
+  },
+  {
+    "revision": "a7a6d441d0a3fd640ea8906421b038cd",
+    "url": "/static/media/EctoLotto.a7a6d441.png"
+  },
+  {
+    "revision": "8a5378217359a48b34a306069bf64316",
+    "url": "/static/media/LottoLoot.8a537821.png"
+  },
+  {
+    "revision": "3e21703e84e402e2f2ec61734c9ce926",
+    "url": "/static/media/boo.3e21703e.png"
+  },
+  {
+    "revision": "b56180103c8fc641eff9755d1b45f189",
+    "url": "/static/media/boo.banker.b5618010.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "f7ca071f1485e8b83f70e609d10571a2",
+    "url": "/static/media/pool.bbra-eth.f7ca071f.png"
+  },
+  {
+    "revision": "58e4df746dae2be2a8bf40890711ddd0",
+    "url": "/static/media/pool.boo-ecto.58e4df74.png"
+  },
+  {
+    "revision": "d7c78cd1ef7040e10d835e64eb4528af",
+    "url": "/static/media/pool.boo-eth.d7c78cd1.png"
+  },
+  {
+    "revision": "764d32f9a37cd5debe5add4a82e92818",
+    "url": "/static/media/pool.boo-rot.764d32f9.png"
+  },
+  {
+    "revision": "00754e22002e07ba6b22c5b493709216",
+    "url": "/static/media/pool.boo-shroom.00754e22.png"
+  },
+  {
+    "revision": "fe58c56508df6a3c09bccc51a0227750",
+    "url": "/static/media/pool.ecto-eth.fe58c565.png"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
